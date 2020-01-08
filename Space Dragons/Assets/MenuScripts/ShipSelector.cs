c9a44d70b13187dd8942d003e9affdf6b15fbf86
Neko Anstle <nekoanstle@gmail.com>
2020-01-08 16:56:27 -0700

    Shipyard Menu V1

---
+++ Space Dragons/Assets/MenuScripts/ShipSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSelector : MonoBehaviour
+{
+    public GameObject ShipMenu;
+
+
+    public void OpenMenu()
+    {
+        ShipMenu.SetActive(!ShipMenu.activeSelf);
+
+    }
+
+}
