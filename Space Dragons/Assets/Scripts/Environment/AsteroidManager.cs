9d27a567bbb6241fa95ef8fdfba462ae4a7d07a2
Rowan <emilyremy@gmail.com>
2020-01-09 14:57:05 -0700

    Asteroid Fixes

--- Space Dragons/Assets/Scripts/Environment/AsteroidManager.cs
+++ Space Dragons/Assets/Scripts/Environment/AsteroidManager.cs
@@ -6,23 +6,23 @@ public class AsteroidManager : MonoBehaviour
 {
     [SerializeField] AsteroidCluster asteroidClusterPrefab = null;
     float worldSize = 2000f;
-    public int ClusterMinimum = 300;
-    public int ClusterMaximum = 500;
+    public int ClusterMinimum = 500;
+    public int ClusterMaximum = 800;
     public List<AsteroidCluster> asteroidClusters = new List<AsteroidCluster>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < Random.Range(ClusterMinimum, ClusterMaximum); i++)
+        {
+            Vector2 location = new Vector2(Random.Range(-worldSize, worldSize), Random.Range(-worldSize, worldSize)); //select spot for cluster
+            asteroidClusters.Add(Instantiate(asteroidClusterPrefab, location, Quaternion.identity, transform));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < Random.Range(ClusterMinimum, ClusterMaximum); i++)
-        {
-            Vector2 location = new Vector2(Random.Range(-worldSize, worldSize), Random.Range(-worldSize, worldSize)); //select spot for cluster
-           asteroidClusters.Add(Instantiate(asteroidClusterPrefab, location, Quaternion.identity, transform));
-        }
+
     }
 }
