57d37d7b3a9fc9bbf4d16e79ee3673203a9492d4
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2020-01-09 14:44:46 -0700

    Updats

---
+++ Space Dragons/Assets/Scripts/Turret/RustyOldTurret.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RustyOldTurret : Turret
+{
+    private new void Awake()
+    {
+        base.Awake();
+    }
+
+    void Update()
+    {
+        if (enemies.Count > 0)
+        {
+            Attack();
+        }
+    }
+
+    public override void Attack()
+    {
+        // Find closest enemy... BLAST'EM
+        Enemy targetEnemy = enemies.Peek();
+
+        if (targetEnemy)
+        {
+            attackTimer += Time.deltaTime;
+
+            if (attackTimer > attackSpeed)
+            {
+                // Shoot bullet
+                attackTimer = 0;
+            }
+        }
+    }
+
+
+}
---
+++ Space Dragons/Assets/Scripts/Turret/Turret.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Turret : MonoBehaviour
+{
+    public float damage = 5;
+    public float range = 1.0f;
+    public float attackSpeed = 0.25f;
+
+    protected float attackTimer = 0.0f;
+
+    protected Queue<Enemy> enemies = new Queue<Enemy>();
+    public abstract void Attack();
+
+    protected void Awake()
+    {
+        GetComponent<CircleCollider2D>().radius = range;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = null;
+
+        collision.gameObject.TryGetComponent(out enemy);
+
+        if (enemy)
+        {
+            enemies.Enqueue(enemy);
+        }
+    }
+}
