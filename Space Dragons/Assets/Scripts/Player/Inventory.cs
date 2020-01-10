8503d4f789face795e5d2287d6b73e996bf3a5d4
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2020-01-10 15:31:14 -0700

    Inclusion of Item.cs and Update of Inventory.cs

--- Space Dragons/Assets/Scripts/Player/Inventory.cs
+++ Space Dragons/Assets/Scripts/Player/Inventory.cs
@@ -1,18 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Dictionary<Item, int> items = new Dictionary<Item, int>();
+    
+    public void AddItem(Item item, int num)
     {
-        
+        List<Item> itemsTemp = items.Keys.ToList();
+        for (int i = 0; i < items.Keys.Count; i++)
+        {
+            if(itemsTemp[i] == item)
+            {
+                items[item] += num;
+                break;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveItem(Item item, int num)
     {
-        
+        List<Item> itemsTemp = items.Keys.ToList();
+        for (int i = 0; i < items.Keys.Count; i++)
+        {
+            if (itemsTemp[i] == item)
+            {
+                if(items[item] >= num)
+                {
+                    items[item] -= num;
+                }
+                else
+                {
+                    // You don't have that many
+                }
+                break;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        List<Item> itemsTemp = items.Keys.ToList();
+        for (int i = 0; i < items.Keys.Count; i++)
+        {
+            Item item = itemsTemp[i];
+            if (items[item] <= 0)
+            {
+                items.Remove(item);
+            }
+        }
+
+        UpdateDisplay();
+    }
+
+    public void UpdateDisplay()
+    {
+
     }
 }
