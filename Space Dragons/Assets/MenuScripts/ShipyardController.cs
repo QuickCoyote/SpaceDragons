4141792eec28c5bca20cbcc6dfceddcb4a65c150
Neko Anstle <nekoanstle@gmail.com>
2020-01-13 12:58:46 -0700

    Completed Shipyard Shop
    Shipyard UI and wiring for the connections have been completed

--- Space Dragons/Assets/MenuScripts/ShipyardController.cs
+++ Space Dragons/Assets/MenuScripts/ShipyardController.cs
@@ -2,21 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ShipyardController : MonoBehaviour
 {
-    public List<GameObject> Ships;
+    public GameObject MotherShip;
+    public List<ShipTest> Ships;
+    public List<ShipTest> ShopShips;
+    public List<ShipTest> CommonShips;
+    public List<ShipTest> RareShips;
+    public List<ShipTest> EpicShips;
+    [Range(0, 2)] public int ShopDifficulty;
     public GameObject ShipScrollContent;
+    public GameObject ShopShipScrollContent;
     public GameObject ShipButtonPrefab;
     public GameObject ShipMenu;
     public GameObject ShopMenu;
+    public TextMeshProUGUI ShipCounter;
+    public TextMeshProUGUI ShopTimer;
+    public GameObject FullOnShipsMessage;
+
+    int NumOfShips;
+    float Timer = 0;
+    float MaxTime = 300;
+    int selectedPurchase = 0;
 
     private void Start()
     {
+        ShipyardShipSetup();
+        ShipyardShopSetup();
 
+    }
+
+    private void Update()
+    {
+        if(Timer > 0)
+        {
+            int minutes = (int)Timer / 60;
+            int seconds = (int)Timer % 60;
+            ShopTimer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+            //ShopTimer.text = string.Format("{0d1:##}", ShopTimer.text);
+            Timer -= 1 * Time.deltaTime;
+        }
+        else if (Timer <= 0)
+        {
+            ShopShips = new List<ShipTest>();
+            ShipyardShopSetup();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            Timer = 5;
+        }
+    
+        ShipCounter.text = NumOfShips + "/" + Ships.Count;
+    }
+
+    public void ShipyardShipSetup()
+    {
         foreach (Transform child in ShipScrollContent.transform)
         {
             Destroy(child.gameObject);
+            NumOfShips = 0;
         }
 
         for (int i = 0; i < Ships.Count; i++)
@@ -27,17 +74,126 @@ public class ShipyardController : MonoBehaviour
             ShipSelector selector = button.GetComponent<ShipSelector>();
             selector.ShipMenu = ShipMenu;
             selector.ShopMenu = ShopMenu;
+            selector.controller = this;
             if (Ships[i] != null)
             {
+                NumOfShips++;
                 button.image.sprite = Ships[i].GetComponent<ShipTest>().ShipSprite;
                 selector.IsSlotFilled = true;
+                selector.SelectedShip = Ships[i].GetComponent<ShipTest>();
             }
             button.onClick.AddListener(delegate { selector.OpenMenu(); });
-            obj.transform.parent = ShipScrollContent.transform;
+            obj.transform.SetParent(ShipScrollContent.transform);
 
+        }
+    }
+
+    public void ShipyardShopSetup()
+    {
+        foreach (Transform child in ShopShipScrollContent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        if (Timer <= 0)
+        {
+            if(ShopDifficulty == 0)
+            {
+                GenerateShopInventory(80, 99);
+            }
+            else if(ShopDifficulty == 1)
+            {
+                GenerateShopInventory(40, 95);
+            }
+            else
+            {
+                GenerateShopInventory(20, 90);
+            }
+            Timer = MaxTime;
+        }
+
+        for (int i = 0; i < ShopShips.Count; i++)
+        {
+            GameObject obj = Instantiate(ShipButtonPrefab);
+            Button button = obj.GetComponentInChildren<Button>();
+            if (ShopShips[i] != null)
+            {
+                button.image.sprite = ShopShips[i].GetComponent<ShipTest>().ShipSprite;
+            }
+            obj.transform.SetParent(ShopShipScrollContent.transform);
         }
 
     }
 
+    private void GenerateShopInventory(int RareProbability, int EpicProbability)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            float randNum = Random.Range(0.0f, 100.0f);
+            if(randNum > EpicProbability)
+            {
+                //Add Random Epic to Shop List
+                int rand = Random.Range(0, 5);
+                ShipTest EpicShip = EpicShips[(int)rand];
+                ShopShips.Add(EpicShip);
+            }
+            else if(randNum > RareProbability)
+            {
+                //Add Random Rare to Shop List
+                int rand = Random.Range(0, 5);
+                ShipTest RareShip = RareShips[(int)rand];
+                ShopShips.Add(RareShip);
+            }
+            else
+            {
+                //Add Random Common to Shop List
+                int rand = Random.Range(0, 5);
+                ShipTest CommonShip = CommonShips[(int)rand];
+                ShopShips.Add(CommonShip);
+            }
+        }
+    }
 
+    public void SelectionIncrement()
+    {
+        selectedPurchase++;
+    }
+
+    public void SelectionDecrement()
+    {
+        selectedPurchase--;
+    }
+
+    public void Purchase()
+    {
+        if(NumOfShips != Ships.Count)
+        {
+            ShipTest purchase = ShopShips[selectedPurchase];
+            for (int i = 0; i < Ships.Count; i++)
+            {
+                if(Ships[i] == null)
+                {
+                    Ships[i] = purchase;
+                    break;
+                }
+            }
+            ShopShips[selectedPurchase] = null;
+            ShipyardShipSetup();
+            ShipyardShopSetup();
+        }
+        else
+        {
+            //Maybe some kind of ERROR message to let the player know they're full on ships
+        }
+    }
+
+    public void TradeIn()
+    {
+        //TRADE IN MOTHERSHIP FOR SHIPYARD'S SHIP
+    }
+
+    public void Repair()
+    {
+        //IMPLEMENT MOTHERSHIP REPAIR
+    }
 }
