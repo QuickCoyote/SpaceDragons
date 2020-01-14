10fafe47a01f9791a15938da89f835fe029e5002
Rowan <emilyremy@gmail.com>
2020-01-14 16:50:36 -0700

    Added Smart Enemy

---
+++ Space Dragons/Assets/Scripts/Enemy/SmartEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartEnemy : Enemy
+{
+    Ship playerShip;
+    protected override void Attack()
+    {
+        if (IsPlayerInSight())
+        {
+            shootingTimer -= Time.deltaTime;
+            if (shootingTimer < 0.0f)
+            {
+                shootingTimer = shootingSpeed;
+                if (projectile)
+                {
+                    GameObject projectileGO = (Instantiate(projectile, gunNozzle.transform.position, gunNozzle.transform.rotation, null) as GameObject);
+                    Projectile p = projectileGO.GetComponent<Projectile>();
+                    p.parentobj = gunNozzle;
+                    p.damage = attackDamage;
+                    p.Fire();
+                }
+            }
+        }
+    }
+
+    protected override void Move()
+    {
+        if (!playerShip) playerShip = Player.GetComponentInParent<Ship>();
+
+        target = playerShip.bodyPartObjects[playerShip.bodyPartObjects.Count - 1].transform.position;
+        Vector3 direction = target - transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
+        transform.Translate(transform.up * speed * Time.smoothDeltaTime, Space.World);
+    }
+}
