2644448653b204bd65c51da8f04cecc789cef29f
Rowan <emilyremy@gmail.com>
2020-01-15 13:11:41 -0700

    sorting folders (big commit)

---
+++ Space Dragons/Assets/Scripts/MenuScripts/ShipSelector.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipSelector : MonoBehaviour
+{
+    public GameObject ShipMenu;
+    public GameObject ShopMenu;
+    public Turret SelectedShip;
+    public ShipyardController controller;
+    public bool IsSlotFilled = false;
+
+    public void OpenMenu()
+    {
+        if(IsSlotFilled)
+        {
+            ShipMenu.SetActive(true);
+            ShopMenu.SetActive(false);
+
+            foreach (Transform child in ShipMenu.GetComponentsInChildren<Transform>())
+            {
+                if(child.tag == "ShipyardShip")
+                {
+                    child.GetComponent<Image>().sprite = SelectedShip.GetComponent<SpriteRenderer>().sprite;
+                }
+                if(child.tag == "SellButton")
+                {
+                    Button button = child.GetComponent<Button>();
+                    button.onClick.RemoveAllListeners();
+                    button.onClick.AddListener(delegate { Sell(10); });
+                    button.onClick.AddListener(delegate { Repair(); });
+                    button.onClick.AddListener(delegate { Upgrade(); });
+                }
+            }
+            
+        }
+        else
+        {
+            ShopMenu.SetActive(true);
+            ShipMenu.SetActive(false);
+        }
+
+    }
+
+    void Sell(int sellPrice)
+    {
+        IsSlotFilled = false;
+        ShipMenu.SetActive(false);
+        controller.Ships.Remove(SelectedShip);
+        controller.Ships.Add(null);
+        controller.ShipyardShipSetup();
+        //PLACE MONEY ADDING HERE
+    }
+
+    void Repair()
+    {
+        //SET SHIP HEALTH TO MAXIMUM
+        controller.ShipyardShipSetup();
+    }
+
+    void Upgrade()
+    {
+        //UPGRADE SHIP SOMEHOW?
+        controller.ShipyardShipSetup();
+    }
+
+}
---
+++ Space Dragons/Assets/Scripts/MenuScripts/ShipTest.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipTest : MonoBehaviour
+{
+    public Sprite ShipSprite;
+    public int SellPrice = 10;
+}
