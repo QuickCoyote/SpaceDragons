35fa2ec9685cd6cad3c34273f54a455a3146086e
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2020-01-16 16:48:37 -0700

    Updat

--- Space Dragons/Assets/Scripts/CameraZoom.cs
+++ Space Dragons/Assets/Scripts/CameraZoom.cs
@@ -18,11 +18,14 @@ public class CameraZoom : MonoBehaviour
     void FixedUpdate()
     {
         targetGroup.m_Targets = new CinemachineTargetGroup.Target[player.bodyPartTransforms.Count];
-        for(int i = 0; i < player.bodyPartTransforms.Count; i++)
+        for (int i = 0; i < player.bodyPartTransforms.Count; i++)
         {
-            targetGroup.m_Targets[i].target = player.bodyPartTransforms[i].transform;
-            targetGroup.m_Targets[i].weight = 1f;
-            targetGroup.m_Targets[i].radius = player.bodyPartObjects[i].GetComponentInChildren<SpriteRenderer>().sprite.bounds.max.magnitude;
+            if (player.bodyPartTransforms[i] != null)
+            {
+                targetGroup.m_Targets[i].target = player.bodyPartTransforms[i].transform;
+                targetGroup.m_Targets[i].weight = 1f;
+                targetGroup.m_Targets[i].radius = player.bodyPartObjects[i].GetComponentInChildren<SpriteRenderer>().sprite.bounds.max.magnitude;
+            }
         }
     }
 }
