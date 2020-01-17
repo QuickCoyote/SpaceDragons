6b2b446b67f20557a6f383d35ee476a3051cbf04
Rowan <emilyremy@gmail.com>
2020-01-17 14:46:24 -0700

    Enemy Damage Animations

--- Space Dragons/Assets/Scripts/Enemy/Enemy.cs
+++ Space Dragons/Assets/Scripts/Enemy/Enemy.cs
@@ -12,6 +12,7 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] public float sightDistance;
     [SerializeField] public float attackDamage;
     [SerializeField] public EnemyDifficutlty difficulty;
+    [SerializeField] protected GameObject DamageParticles = null;
 
     public enum EnemyDifficutlty
     {
@@ -34,6 +35,7 @@ public abstract class Enemy : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
         hp = GetComponent<Health>();
+        DamageParticles.SetActive(false);
     }
 
     void FixedUpdate()
@@ -45,6 +47,9 @@ public abstract class Enemy : MonoBehaviour
         {
             FindObjectOfType<WorldManager>().SpawnRandomExplosion(transform.position);
             Destroy(gameObject);
+        } else if (hp.healthCount < hp.healthMax/2 && !DamageParticles.activeSelf)
+        {
+            DamageParticles.SetActive(true);
         }
     }
 
