e3bf532dd5ebc8ea89cd4402c131278d4ea5a583
Rowan <emilyremy@gmail.com>
2020-01-23 14:31:27 -0700

    map line renders

--- Space Dragons/Assets/Scripts/Misc/Map.cs
+++ Space Dragons/Assets/Scripts/Misc/Map.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Map : MonoBehaviour
 {
     public GameObject MainMap;
     public GameObject PlayerIcon;
+    public LineRenderer linerendererprefab;
+    public TextMeshProUGUI shortestdistanceReadout = null;
+
+    ShipyardController[] Shipyards;
+    List<LineRenderer> linerenderers = new List<LineRenderer>();
+    GameObject player;
 
     public void MapOpen()
     {
@@ -17,6 +24,30 @@ public class Map : MonoBehaviour
     {
         MainMap.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+    private void Start()
+    {
+       Shipyards = FindObjectsOfType<ShipyardController>();
+        foreach (ShipyardController g in Shipyards)
+        {
+            linerenderers.Add(Instantiate(linerendererprefab, transform));
+        }
+        player = WorldManager.Instance.Player;
+    }
+
+    private void Update()
+    {
+        float shortestDistance = 50000;
+        for (int i  = 0; i < linerenderers.Count; i++)
+        {
+            linerenderers[i].positionCount = 2;
+            linerenderers[i].SetPosition(0, Shipyards[i].transform.position - Vector3.forward); 
+            linerenderers[i].SetPosition(1, player.transform.position - Vector3.forward);
+            if (Vector3.Distance(player.transform.position, Shipyards[i].transform.position) < shortestDistance) shortestDistance = Vector3.Distance(player.transform.position, Shipyards[i].transform.position);
+
+        }
+        shortestdistanceReadout.text = shortestDistance.ToString("000km");
     }
 
 }
