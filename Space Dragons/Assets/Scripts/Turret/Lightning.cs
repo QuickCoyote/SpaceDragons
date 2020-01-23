8236dfe2569f9f9e2534df6407c01c5338d811bb
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2020-01-23 16:52:57 -0700

    Big Updat

--- Space Dragons/Assets/Scripts/Turret/Lightning.cs
+++ Space Dragons/Assets/Scripts/Turret/Lightning.cs
@@ -6,34 +6,51 @@ public class Lightning : MonoBehaviour
 {
     public Transform target = null;
 
+
+    float lifetime = 0.25f;
+    float dt = 0;
     LineRenderer lr = null;
 
     void Start()
     {
         gameObject.AddComponent<LineRenderer>();
         lr = GetComponent<LineRenderer>();
-        Debug.Log("Drawin it >:D");
         lr.material = WorldManager.Instance.lightningMat;
-        lr.startWidth = 0.15f;
-        lr.endWidth = 0.15f;
+        lr.startWidth = 2;
+        lr.endWidth = 2;
         lr.positionCount = 10;
         lr.sortingLayerName = "Default";
         lr.sortingOrder = 95;
+        lr.startColor = Color.cyan;
+        lr.endColor = Color.cyan;
 
         Vector3 TotalVector = target.position - transform.position;
 
         Vector3[] Segments = new Vector3[10];
 
-        for (int i = 0; i < 10; i++)
+        Segments[0] = transform.position;
+        Segments[9] = target.transform.position;
+        for (int i = 1; i < 9; i++)
         {
             Vector3 normalized = TotalVector/10;
             float rand = Random.Range(-1.0f, 1.0f);
-            Segments[i] = normalized * (i+1) + (new Vector3(normalized.y, normalized.x, 0) * rand);
+            Segments[i] = normalized * (i+1) + (new Vector3(normalized.y, normalized.x, 0) * rand) + transform.position;
+            Segments[i] = new Vector3(Segments[i].x, Segments[i].y, -1);
             Debug.Log(Segments[i]);
         }
 
         lr.SetPositions(Segments);
         lr.enabled = true;
+    }
+
+    void Update()
+    {
+        dt += Time.deltaTime;
+
+        if(dt > lifetime)
+        {
+            RemoveLightning();
+        }
     }
 
     public void RemoveLightning()
--- Space Dragons/Assets/Scripts/Turret/LightningTurret.cs
+++ Space Dragons/Assets/Scripts/Turret/LightningTurret.cs
@@ -26,7 +26,7 @@ public class LightningTurret : Turret
         {
             Vector3 direction = enemy.transform.position - rotateBoi.gameObject.transform.position;
             float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-            if (angle < 5 && angle > -5)
+            if (angle < 15 && angle > -15)
             {
                 enemiesShocked = 1;
                 ShockNext(enemy);
