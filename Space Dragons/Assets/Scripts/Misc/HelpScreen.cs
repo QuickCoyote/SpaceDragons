5da215b08c9a011c23755ad513a45d278393a317
Rowan <emilyremy@gmail.com>
2020-01-28 13:31:20 -0700

    Help Panels 2/3

---
+++ Space Dragons/Assets/Scripts/Misc/HelpScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScreen : MonoBehaviour
+{
+    [SerializeField] GameObject HelpScreenObject = null;
+
+    public void ToggleHelpScreen()
+    {
+        HelpScreenObject.SetActive(!HelpScreenObject.activeSelf);
+    }
+
+    public void CloseHelpScreen()
+    {
+        HelpScreenObject.SetActive(false);
+    }
+
+}
