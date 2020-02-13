43c97c01edc96187628826c48209bd983c77990e
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2020-02-13 16:46:47 -0700

    zdf;ohba;ekjrvbad;.,mfvgzbd;db.fv,adgx'ozhl/k.vserjdgf[o' mrd

---
+++ Space Dragons/Assets/Scripts/Enemy/Human Boss/LaserBeam.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+    List<Health> objectsToDealDamageTo = new List<Health>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Health health = null;
+
+        collision.TryGetComponent(out health);
+
+        if (health)
+        {
+            if (health != transform.parent.GetComponent<Health>())
+            {
+                objectsToDealDamageTo.Add(health);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        Vector3[] points = new Vector3[2];
+
+        points[0] = new Vector3(transform.parent.transform.position.x, transform.parent.transform.position.y, -1);
+        Vector3 newUp = transform.parent.transform.up * transform.parent.GetComponent<HumanBossEnemy>().LaserFireDistance;
+        points[1] = new Vector3(newUp.x, newUp.y, -1);
+
+        LineRenderer line = GetComponent<LineRenderer>();
+        line.SetPositions(points);
+
+        foreach (Health health in objectsToDealDamageTo)
+        {
+            health.healthCount -= HumanBossEnemy.laserBeamDamage * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Health health = null;
+
+        collision.TryGetComponent(out health);
+
+        if (health)
+        {
+            if (objectsToDealDamageTo.Contains(health))
+            {
+                objectsToDealDamageTo.Remove(health);
+            }
+        }
+    }
+}
---
+++ Space Dragons/Assets/Scripts/Supporting/GameObjectEx.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public static class GameObjectEx
+{
+    public static void DrawCircle(this GameObject container, float radius, float lineWidth, float variance, Material mat)
+    {
+        var segments = 360;
+        var line = container.GetComponent<LineRenderer>();
+        if (!line)
+        {
+            line = container.AddComponent<LineRenderer>();
+        }
+
+        line.useWorldSpace = false;
+        line.startWidth = lineWidth;
+        line.endWidth = lineWidth;
+        line.positionCount = segments + 1;
+        line.material = mat;
+
+        var pointCount = segments + 1; // add extra point to make startpoint and endpoint the same to close the circle
+        var points = new Vector3[pointCount];
+
+        for (int i = 0; i < pointCount; i++)
+        {
+            var rad = Mathf.Deg2Rad * (i * 360f / segments);
+            points[i] = new Vector3(Mathf.Sin(rad) * radius, Mathf.Cos(rad) * radius, -1);
+            points[i] += (points[i] - container.transform.position).normalized * variance;
+        }
+
+        line.SetPositions(points);
+    }
+}
