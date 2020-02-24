fcd8b2965b739c37526680cd0c32dbcc963cfe26
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2020-02-24 16:15:44 -0700

    Fixes

--- Space Dragons/Assets/Scripts/ManagersAndSingletons/TrackingManager.cs
+++ Space Dragons/Assets/Scripts/ManagersAndSingletons/TrackingManager.cs
@@ -63,45 +63,6 @@ public class TrackingManager : Singleton<TrackingManager>
         return (MainMap.highlightIcon.transform.position - player.transform.position).magnitude;
     }
 
-    private void Update()
-    {
-        Vector3 direction = Vector3.zero;
-        if(MainMap.highlightIcon.activeSelf)
-        {
-            //Check where to rotate minimap tracker
-            direction = MainMap.highlightIcon.transform.position - player.transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            MiniMapTargetIcon.transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
-            MiniMapTargetIcon.enabled = (direction.magnitude > 10.0f);
-        }
-        else
-        {
-            MiniMapTargetIcon.enabled = false;
-        }
-
-        //Check where to rotate enemy tracker
-        float closestEnemy = 50000;
-        Vector3 enemydirection = Vector3.zero;
-        foreach (Enemy e in FindObjectsOfType<Enemy>())
-        {
-            float distance = Vector3.Distance(e.transform.position, player.transform.position);
-            if (distance < closestEnemy)
-            {
-                closestEnemy = distance;
-                enemydirection = e.transform.position - player.transform.position;
-            }
-        }
-
-        float enemyangle = Mathf.Atan2(enemydirection.y, enemydirection.x) * Mathf.Rad2Deg;
-        EnemyIcon.transform.rotation = Quaternion.AngleAxis(enemyangle + 90, Vector3.forward);
-        EnemyIcon.enabled = (enemydirection.magnitude > 10.0f);
-    }
-
-    public float ReturnDistanceToTracker()
-    {
-        return (MainMap.highlightIcon.transform.position - player.transform.position).magnitude;
-    }
-
     public int ReturnETA()
     {
         return Mathf.CeilToInt((Vector3.Distance(MainMap.highlightIcon.transform.position, player.transform.position) / WorldManager.Instance.Ship.speed));
