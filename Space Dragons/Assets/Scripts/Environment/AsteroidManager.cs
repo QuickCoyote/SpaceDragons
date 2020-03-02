3b83f99f3db939246ecf2a563c84c5d3cf584134
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2020-03-02 13:09:35 -0700

    Push

--- Space Dragons/Assets/Scripts/Environment/AsteroidManager.cs
+++ Space Dragons/Assets/Scripts/Environment/AsteroidManager.cs
@@ -13,34 +13,112 @@ public class AsteroidManager : Singleton<AsteroidManager>
 
     public int AsteroidsDestroyed = 0;
 
+    WorldManager worldManager;
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag = "";
+        public GameObject objectPrefab;
+        public int maxNumOfObject;
+    }
+
+    public List<Pool> Pools = new List<Pool>();
+    public Dictionary<string, Queue<GameObject>> objectPools = new Dictionary<string, Queue<GameObject>>();
     void Start()
     {
-        Vector2 worldSize = WorldManager.Instance.WorldCorner.position;
+        worldManager = WorldManager.Instance;
+        foreach(Pool pool in Pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.maxNumOfObject; i++)
+            {
+                GameObject obj = Instantiate(asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)]);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            objectPools.Add(pool.tag, objectPool);
+        }
+
         for (int i = 0; i < ClusterNum; i++)
         {
-            Vector3 location = new Vector3(Random.Range(-worldSize.x, worldSize.x), Random.Range(-worldSize.y, worldSize.y),0); //select spot for cluster
+            Vector3 location = new Vector3(Random.Range(-150, 150), Random.Range(-150, 150),0);
             for (int j = 0; j < Random.Range(AsteroidMinimum, AsteroidMaximum); j++)
             {
-                Instantiate(asteroidPrefabs[Random.Range(0, 3)], location + new Vector3(Random.value, Random.value, 0), Quaternion.identity, null); //Select smaller locations for each asteroid
+                SpawnFromPool("Asteroid", location + new Vector3(Random.value, Random.value, 0), Quaternion.identity);
             }
         }
     }
 
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if(!objectPools.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + "doesn't exist.");
+            return null;
+        }
+
+        GameObject objectToSpawn = objectPools[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        if(Vector3.Distance(objectToSpawn.transform.position, worldManager.transform.position) < 50)
+        {
+            worldManager.AsteroidsToRender.Add(objectToSpawn);
+        }
+
+        objectPools[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
     private void FixedUpdate()
     {
+        float val = Random.Range(-1, 1);
+        float val2 = Random.Range(-1, 1);
+        if (val < 0)
+        {
+            val = -1;
+        }
+        else
+        {
+            val = 1;
+        }
+        if (val2 < 0)
+        {
+            val2 = -1;
+        }
+        else
+        {
+            val2 = 1;
+        }
+        Vector3 location = new Vector3(Random.Range(50, 100) * val, Random.Range(50, 100) * val2, 0);
+
         if (AsteroidsDestroyed > 8)
         {
-            Vector3 location = new Vector3(Random.Range(-1000, 1000), Random.Range(-1000, 1000), 0); //select spot for cluster
+            location += worldManager.Head.transform.position;
             for (int j = 0; j < Random.Range(AsteroidMinimum, AsteroidMaximum); j++)
             {
-               Instantiate(asteroidPrefabs[Random.Range(0, 3)], location + new Vector3(Random.value, Random.value, 0), Quaternion.identity, null); //Select smaller locations for each asteroid
+                SpawnFromPool("Asteroid", location, Quaternion.identity);
             }
             AsteroidsDestroyed = 0;
+        }
+
+        foreach(GameObject asteroid in objectPools["Asteroid"])
+        {
+            if(Vector3.Distance(asteroid.transform.position, worldManager.transform.position) > 150)
+            {
+                asteroid.transform.position = location;
+            }
         }
     }
 
     public void SpawnAsteroidDestruction(Vector3 target)
     {
-        Instantiate(asteroidBreakupPrefab, target, Quaternion.identity, null);
+        SpawnFromPool("Asteroid", target, Quaternion.identity);
     }
 }
