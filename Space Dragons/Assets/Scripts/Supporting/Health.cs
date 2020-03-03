8d659e72edd8c5a1d02eadcc1df195bcb5341682
Rowan <emilyremy@gmail.com>
2020-03-03 15:55:07 -0700

    Health updat

--- Space Dragons/Assets/Scripts/Supporting/Health.cs
+++ Space Dragons/Assets/Scripts/Supporting/Health.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] public GameObject healthbarObj = null;
-    Slider healthbar = null;
-    public float healthMax = 1.0f;
-    public float healthCount = 0.0f;
+    [SerializeField] public Transform barTransform = null;
+    [SerializeField] public SpriteRenderer hbRenderer = null;
 
+    Slider healthbar = null;
     public int objectIndex = 0;
 
+    public float healthMax = 1.0f;
+    public float healthCount = 0.0f;
     Animator an = null;
 
     public void Start()
@@ -20,12 +20,18 @@ public class Health : MonoBehaviour
         if (healthbarObj)
         {
             healthbar = healthbarObj.GetComponent<Slider>();
-        }
-        healthCount = healthMax;
-        if (healthbar)
+            if (healthbar)
+            {
+                healthbar.maxValue = healthMax;
+            }
+        } else
         {
-            healthbar.maxValue = healthMax;
+
         }
+        
+        healthCount = healthMax;
+
+
     }
 
     public void ResetHealth()
@@ -57,9 +63,16 @@ public class Health : MonoBehaviour
                 healthbar = healthbarObj.GetComponent<Slider>();
             }
         }
+        else
+        {
+            barTransform.localScale = new Vector3(healthCount / healthMax, 1, 1);
+            barTransform.localPosition = new Vector3( hbRenderer.bounds.extents.x * (healthCount / healthMax) - hbRenderer.bounds.extents.x, 0, 0);
+        }
+
         if (healthCount > healthMax)
         {
             healthCount = healthMax;
         }
     }
+
 }
